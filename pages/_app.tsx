de869997e60457883c65e5a4e0e5b3ff5d5d344e
Shadab Khan <shadab.2005@gmail.com>
2023-04-30 11:17:59 +0530

    chore: basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WagmiConfig, createClient, configureChains, mainnet } from "wagmi"
+import { publicProvider } from 'wagmi/providers/public'
+import { Session } from "next-auth"
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const { chains, provider } = configureChains(
+  [mainnet],
+  [publicProvider()]
+)
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+})
+
+export default function App({ Component, pageProps }: AppProps<{ session: Session;}>) {
+  return <WagmiConfig client={client}><SessionProvider session={pageProps.session} refetchInterval={0}><Component {...pageProps} /></SessionProvider></WagmiConfig>
 }
